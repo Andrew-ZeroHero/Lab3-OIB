e98272cf8dc23f19f7f2ab494dccfb91ab354ea5
Дрон <93979827+Andrew-ZeroHero@users.noreply.github.com>
2021-12-13 10:58:53 +0400

    Update lab.py

--- lab.py
+++ lab.py
@@ -11,7 +11,7 @@ from cryptography.hazmat.primitives.ciphers import Cipher, algorithms
 
 with open('settings.json') as json_file:
     json_data = json.load(json_file)
-
+    
 initial_path = json_data["initial_file"]
 encrypted_path = json_data["encrypted_file"]
 decrypted_path = json_data["decrypted_file"]
@@ -19,8 +19,14 @@ symmetric_path = json_data["symmetric_key"]
 public_path = json_data["public_key"]
 secret_path = json_data["secret_key"]
 
-def key_generation(path_to_symmetric_key: str, path_to_public_key: str, path_to_secret_key: str) -> None:
+def key_generation(path_symmetric_key: str, path_public_key: str, path_secret_key: str) -> None:
 
+    """
+    :param path_symmetric_key: Путь к файлу, где лежит зашифрованный симметричный ключ
+    :param path_public_key: Путь к файлу с публичным ключом
+    :param path_secret_key: Путь к файлу с секретным ключом
+    """
+    
     symmetric_key = ChaCha20Poly1305.generate_key()
 
     keys = rsa.generate_private_key(public_exponent=65537, key_size=2048)
@@ -29,55 +35,97 @@ def key_generation(path_to_symmetric_key: str, path_to_public_key: str, path_to_
     privat_pem = privat_k.private_bytes(encoding=serialization.Encoding.PEM,
                                         format=serialization.PrivateFormat.TraditionalOpenSSL,
                                         encryption_algorithm=serialization.NoEncryption())
-    with open(path_to_secret_key, 'wb') as file:
+    with open(path_secret_key, 'wb') as file:
         file.write(privat_pem)
 
     public_k = keys.public_key()
     public_pem = public_k.public_bytes(encoding=serialization.Encoding.PEM,
                                        format=serialization.PublicFormat.SubjectPublicKeyInfo)
-    with open(path_to_public_key, 'wb') as file:
+    with open(path_public_key, 'wb') as file:
         file.write(public_pem)
 
     enc_symmetrical_key = public_k.encrypt(symmetric_key, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),
                                                                        algorithm=hashes.SHA256(), label=None))
-    with open(path_to_symmetric_key, 'wb') as file:
+    with open(path_symmetric_key, 'wb') as file:
         file.write(enc_symmetrical_key)
-# Переделать!
-key = os.urandom(32)
-nonce = os.urandom(16)
-print(type(key))
-print(key)
-print(nonce)
-file_name = 'symmetric_key.txt'
-with open(file_name, 'wb') as key_file:
-  key_file.write(key)
- 
-file_name = 'symmetric_nonce.txt'
-with open(file_name, 'wb') as nonce_file:
-  nonce_file.write(nonce)
+        
+def decrypt_symmetric_key(path_symmetric_key: str, path_secret: str):
+    
+    """
+    :param path_symmetric_key: Путь к файлу, где лежит зашифрованный симметричный ключ
+    :param path_secret_key: Путь к файлу с секретным ключом
+    """
+    
+    with open(path_symmetric_key, 'rb') as file:
+        enc_symmetrical_key = file.read()
+    with open(path_secret_key, 'rb') as file:
+        privat_k = serialization.load_pem_private_key(file.read(), password=None)
+    symmetrical_key = privat_k.decrypt(enc_symmetrical_key, padding.OAEP(mgf=padding.MGF1(algorithm=hashes.SHA256()),
+                                                                         algorithm=hashes.SHA256(), label=None))
+    return symmetrical_key
 
-txt_file = '/content/text.txt'
-with open(txt_file, 'rb') as read_file:
-  text = read_file.read()
-algorithm = algorithms.ChaCha20(key, nonce)
-cipher = Cipher(algorithm, mode=None)
-encryptor = cipher.encryptor()
-enc_txt = encryptor.update(text)
 
-print(enc_txt)
-decryptor = cipher.decryptor()
-decryptor.update(enc_txt)
-from cryptography.hazmat.primitives.asymmetric import rsa
-from cryptography.hazmat.primitives import serialization
+def encrypt_file(path_to_initial_file: str, path_to_secret_key: str, path_to_symmetric_key: str,
+                 path_to_encrypt_file: str) -> None:
+    
+    """
+    :param path_to_initial_file: Путь к файлу, в котором находится исходный текст
+    :param path_to_secret_key: Путь к файлу с секретным ключом
+    :param path_to_symmetric_key: Путь к файлу, где лежит зашифрованный симметричный ключ
+    :param path_to_encrypt_file: Путь к файлу, куда будет сохранен зашифрованный текст
+    """
+    
+    symmetrical_key = decrypt_symmetric_key(path_symmetric_key, path_secret_key)
+    with open(path_to_initial_file, 'r', encoding='utf-8') as file:
+        txt = file.read()
+    nonce = os.urandom(16)
+    algorithm = algorithms.ChaCha20(symmetrical_key, nonce)
+    cipher = Cipher(algorithm, mode=None)
+    encrypt = cipher.encryptor()
+    cipher_txt = encrypt.update(bytes(txt, 'utf-8'))
+    res = {'ciphertxt': cipher_txt, 'nonce': nonce}
+    with open(path_to_encrypt_file, 'wb') as file:
+        pickle.dump(res, file)
 
-keys = rsa.generate_private_key(
-    public_exponent=65537,
-    key_size=2048
-)
-private_key = keys
-public_key = keys.public_key()
 
-print(type(private_key))
-print(private_key)
-print(type(public_key))
-print(public_key)
+def decrypt_file(path_to_encrypt_file: str, path_secret_key: str, path_symmetric_key: str,
+                 path_to_decrypted_file: str) -> None:
+    
+    """
+    :param path_to_encrypt_file: Путь к файлу, где находится зашифрованный текст
+    :param path_secret_key: Путь к файлу с секретным ключом
+    :param path_symmetric_key: Путь к файлу, где лежит зашифрованный симметричный ключ
+    :param path_to_decrypted_file: Путь к файлу, куда будет сохранен расшифрованный текст
+    """
+    
+    symmetrical_key = decrypt_symmetric_key(path_symmetric_key, path_secret_key)
+    with open(path_to_encrypt_file, 'rb') as file:
+        cipher_tmp = pickle.load(file)
+    cipher_txt = cipher_tmp['ciphertxt']
+    nonce = cipher_tmp['nonce']
+    algorithm = algorithms.ChaCha20(symmetrical_key, nonce)
+    cipher = Cipher(algorithm, mode=None)
+    decrypt = cipher.decryptor()
+    dec_txt = decrypt.update(cipher_txt) + decrypt.finalize()
+    with open(path_to_decrypted_file, 'w', encoding='utf-8') as file:
+        file.write(dec_txt.decode('utf-8'))
+
+
+parser = argparse.ArgumentParser(description='main.py')
+group = parser.add_mutually_exclusive_group(required=True)
+group.add_argument('-gen', '--generation', type=str, help='Запускает режим генерации ключей', dest='generation')
+group.add_argument('-enc', '--encryption', type=str, help='Запускает режим шифрования', dest='encryption')
+group.add_argument('-dec', '--decryption', type=str, help='Запускает режим дешифрования', dest='decryption')
+args = parser.parse_args()
+
+if args.generation is not None:
+    key_generation(path_symmetric_key, path_public_key, path_secret_key)
+    print('Ключи созданы')
+    
+if args.encryption is not None:
+    encrypt_file(path_initial_file, path_secret_key, path_symmetric_key, path_encrypted_file)
+    print('Файл зашифрован')
+    
+if args.decryption is not None:
+    decrypt_file(path_encrypted_file, path_secret_key, path_symmetric_key, path_decrypted_file)
+    print('Файл расшифрован')
